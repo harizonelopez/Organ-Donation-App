19be8e9f20ee9e240bf4d5339cfedad58bb59d4f
Harizone lopez <harizonelopez23@gmail.com>
2025-03-21 16:34:11 +0300

    Initial commit

---
+++ hospitals/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("province", "hospital_name", "country", "contact_number")}),
+    )
+
+
+admin.site.register(User, UserAdmin)
