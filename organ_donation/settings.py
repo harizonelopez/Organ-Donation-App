2dff3ec966a3ab8a7c39c6add763af311636fc10
Harizone lopez <harizonelopez23@gmail.com>
2025-04-01 16:34:20 +0300

    add email configuration settings using environ for secure management

--- organ_donation/settings.py
+++ organ_donation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,16 @@ AUTH_USER_MODEL = "hospitals.User"
 MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings
+env = environ.Env()
+environ.Env.read_env()  # Reads from a .env file
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
