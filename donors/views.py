61771ebda3de9d51479d80c31b7d28284d4f26fe
Harizone lopez <harizonelopez23@gmail.com>
2025-05-27 12:35:50 +0300

    Remove unused import from views.py for cleaner code

--- donors/views.py
+++ donors/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from hospitals.models import User
 from .models import DonationRequests
 from django.contrib.auth import login, logout, authenticate
